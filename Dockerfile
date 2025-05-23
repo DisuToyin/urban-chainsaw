b248685d01f02823ce7f476443832b7193e84c3b
DisuToyin <disujt@gmail.com>
2025-05-23 11:59:28 +0100

    test

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Base image 
+FROM node:22.11.0
+
+# This sets our working directory of the container to /app 
+WORKDIR /app
+
+COPY package.json .
+# RUN npm install 
+
+ARG NODE_ENV
+RUN if [ "$NODE_ENV" = "development"]; \
+        then npm install; \
+        else npm install --only=production; \
+        fi
+COPY . ./
+
+EXPOSE 5000
+CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const mongoose = require('mongoose');
+const {MONGO_URI} = require("./config/config")
+
+const app = express()
+mongoose.connect(MONGO_URI).then(()=>console.log("Successfully connected to DB")).catch((e)=>console.log(e))
+
+app.get("/", (req, res)=> {
+    res.send("<h2>Hello Everyone Dev!</h2>")
+})
+
+const port = process.env.PORT || 5000
+app.listen(5000, () => console.log("Server is running on port 5000"))

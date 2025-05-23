bd3c2e1c4a54080c6a6c6faa8e54e57b4acffa56
DisuToyin <disujt@gmail.com>
2025-05-23 12:11:39 +0100

    test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const mongoose = require('mongoose');
-const {MONGO_URI} = require("./config/config")
+// const {MONGO_URI} = require("./config/config")
 
 const app = express()
-mongoose.connect("mongodb://disu:mypass@mongo:27017/?authSource=admin").then(()=>console.log("Successfully connected to DB")).catch((e)=>console.log(e))
+// mongoose.connect(`mongodb://disu:mypass@mongo:27017/?authSource=admin`).then(()=>console.log("Successfully connected to DB")).catch((e)=>console.log(e))
 
 app.get("/", (req, res)=> {
     res.send("<h2>Hello Everyone Dev!</h2>")
